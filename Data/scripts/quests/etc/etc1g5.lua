c5198783a16c1021e71057a878acec902de4f15d
Filip Maj <filipmaj@gmail.com>
2022-02-24 20:31:05 -0500

    Adding `Sleepless in Eorzea`, `Bridging the Gap`, and `The Search for Sicksa`.

---
+++ Data/scripts/quests/etc/etc1g5.lua
@@ -0,0 +1,104 @@
+require ("global")
+
+--[[
+
+Quest Script
+
+Name: 	The Search for Sicksa
+Code: 	Etc1g5
+Id: 	110659
+Prereq: Level 10, Any Class
+
+]]
+
+-- Sequence Numbers
+SEQ_000	= 0;  -- Kill Bristletail Marmots.
+SEQ_001	= 1;  -- Talk to Beli.
+
+-- Actor Class Ids
+ENPC_BELI 				= 1001077;
+BNPC_BRISTLETAIL_MARMOT	= 2104022;
+
+-- Quest Markers
+MRKR_BRISTLETAIL_AREA	= 11065901;
+MRKR_BELI				= 11065902;
+
+-- Counters
+COUNTER_MARMOTHIDE		= 0;
+
+-- Quest Details
+OBJECTIVE_MARMOTHIDE	= 8;
+
+function onStart(player, quest)	
+	quest:StartSequence(SEQ_000);
+end
+
+function onFinish(player, quest)
+end
+
+function onStateChange(player, quest, sequence)	
+	if (sequence == SEQ_ACCEPT) then
+		quest:SetENpc(ENPC_BELI, QFLAG_PLATE);
+	elseif (sequence == SEQ_000) then
+        quest:SetENpc(ENPC_BELI);
+		quest:SetENpc(BNPC_BRISTLETAIL_MARMOT);
+	elseif (sequence == SEQ_001) then
+		quest:SetENpc(ENPC_BELI, QFLAG_REWARD);
+	end	
+end
+
+function onTalk(player, quest, npc, eventName)
+	local npcClassId = npc.GetActorClassId();
+	local seq = quest:GetSequence();
+    
+	-- Offer the quest
+	if (npcClassId == ENPC_BELI and seq == SEQ_ACCEPT) then
+		local questAccepted = callClientFunction(player, "delegateEvent", player, quest, "processEventLahonoStart");
+		if (questAccepted == 1) then
+			player:AcceptQuest(quest);
+		end
+		player:EndEvent();
+		return;	
+	-- Quest Progress
+	elseif (seq == SEQ_000) then
+        if (npcClassId == ENPC_BELI) then
+            callClientFunction(player, "delegateEvent", player, quest, "processEventFree");
+		end
+	--Quest Complete
+	elseif (seq == SEQ_001) then
+		if (npcClassId == ENPC_BELI) then
+			callClientFunction(player, "delegateEvent", player, quest, "processEventAfter");
+			callClientFunction(player, "delegateEvent", player, quest, "sqrwa", 200, 1, 1, 9);
+            player:CompleteQuest(quest);
+		end
+	end
+	
+	quest:UpdateENPCs();	
+	player:EndEvent();
+end
+
+-- TODO FINISH THIS
+function onKillBNpc(player, quest, bnpc)
+	if (bnpc == BNPC_BRISTLETAIL_MARMOT) then
+		local counterAmount = quest:GetData():IncCounter(COUNTER_MARMOTHIDE);
+		attentionMessage(player, 51062, 0, counterAmount, 4); -- You obtain <item>
+        if (counterAmount >= OBJECTIVE_MARMOTHIDE) then
+			attentionMessage(player, 25225, quest:GetQuestId()); -- Objectives complete!
+			quest:StartSequence(SEQ_001);
+		end
+	end
+end
+
+function getJournalInformation(player, quest)
+	return quest:GetData():GetCounter(COUNTER_MARMOTHIDE);
+end
+
+function getJournalMapMarkerList(player, quest)
+    local sequence = quest:getSequence();
+    
+    if (sequence == SEQ_000) then
+		return MRKR_BRISTLETAIL_AREA;
+    elseif (sequence == SEQ_001) then
+        return MRKR_BELI;
+    end
+end
